c35e12e5e3583ee2684807484afd7b564e501cfd
Anastasiia <dessertkiev@gmail.com>
2023-06-26 16:51:59 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,16 +1,25 @@
+import { useState } from 'react'; 
 import PropTypes from "prop-types"
+import { Modal } from "../Modal/Modal";
 import css from './ImageGalleryItem.module.css';
 
-export const ImageGalleryItem = ({ link, descr, openModal }) => {
+export const ImageGalleryItem = ({ link, descr, full }) => {
+  const [modal, setModal] = useState(null);
+
   return (
-    <li className={css.ImageGalleryItem} onClick={openModal}>
-      <img className={css['ImageGalleryItem-image']} src={link} alt={descr} />
-    </li>
+    <>
+      <li className={css.ImageGalleryItem} onClick={() => setModal(full)}>
+        <img className={css['ImageGalleryItem-image']} src={link} alt={descr} />
+      </li>
+      {modal && (
+        <Modal link={full} descr={descr} onClose={() => setModal(null)} />
+      )}
+    </>
   );
 };
 
 ImageGalleryItem.propTypes = {
   descr: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
-  openModal: PropTypes.func.isRequired,
+  full: PropTypes.string.isRequired,
 };
