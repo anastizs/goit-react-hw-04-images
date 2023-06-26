043f800d67006166d55d179c7e376ad7ad6e890c
Anastasiia <dessertkiev@gmail.com>
2023-06-26 16:51:56 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,17 +2,16 @@ import PropTypes from 'prop-types';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import css from './ImageGallery.module.css';
 
-export const ImageGallery = ({ images, openModal }) => {
+export const ImageGallery = ({ images }) => {
   return (
     <ul className={css.ImageGallery}>
       {images.map(image => (
         <ImageGalleryItem
-          openModal={() => {
-            openModal(image.id);
-          }}
+          
           key={image.id}
           link={image.webformatURL}
           descr={image.tags}
+          full={image.largeImageURL}
         />
       ))}
     </ul>
@@ -24,6 +23,5 @@ ImageGallery.propTypes = {
     PropTypes.shape({
       id: PropTypes.number.isRequired,
     })
-  ),
-  openModal: PropTypes.func.isRequired,
+  )
 };
